c43eb947badd6cc0a9e4d8f6b5cab77aa29e6cf3
Eros Ferreira <erosferreira00@gmail.com>
2023-10-25 15:11:54 -0300

    commit inicial

---
+++ scripts.js
@@ -0,0 +1,47 @@
+let nomeHeroi = "ThorDraw";
+let XP = 1000;
+let div;
+
+switch(XP){
+    case xp <= 1000:
+    div = "Ferro";
+    break
+
+    case xp <= 1001 && xp >= 2000:
+    div = "Bronze";
+    break
+
+    case xp >= 2001 && xp >= 5000:
+    div = "Prata";
+    break
+
+    case xp >= 5001 && xp <= 7000:
+    div = "Ouro";
+    break
+
+    case xp <= 7001 && xp >= 8000:
+    div = "Platina";
+    break
+
+    case xp <= 8001 && xp >= 9000:
+    div = "Ascendente";
+    break 
+
+    case xp <= 9001 && xp <= 10000:
+    div = "Imortal";
+    break 
+
+    case xp >= 10000:
+    div = "Radiante";
+    break
+
+    default:
+    console.log("Não encontrado na base de dados");
+}
+
+console.log(`${nomeHeroi} é da divisão de ${div} e tem ${XP} de experência.`);
+
+
+
+
+
